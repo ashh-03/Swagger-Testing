d238926a074121722f7c402a195860559fcf5784
ASHISH TIWARY <ashishtiwary003@gmail.com>
2026-06-16 10:03:33 +0530

    Initail Commit

---
+++ src/main/java/com/swaggerApiTesting/swaggerProjectTesting/SwaggerProjectTestingApplication.java
@@ -0,0 +1,13 @@
+package com.swaggerApiTesting.swaggerProjectTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerProjectTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerProjectTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swaggerApiTesting/swaggerProjectTesting/SwaggerProjectTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swaggerApiTesting.swaggerProjectTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerProjectTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
